59d4111c749369beac49a5aead1a50bd2447189e
BishwasXD <gautambishwas7@gmail.com>
2025-01-10 21:01:05 +0545

    feat: can mark task as done

--- bin/todo.sh
+++ bin/todo.sh
@@ -22,6 +22,7 @@ elif [[ "$arg1" == "--help" && -z "$arg2" ]];then
   echo "todo ls: lists all task"
   echo "todo add task_name: add new task" 
   echo "todo rm task_name: remove task"
+  echo "todo -m task_name: mark task done" 
 
 elif [[ "$arg1" == "rm" && -n "$arg2" ]]; then
   task_name="$arg2"
@@ -37,9 +38,29 @@ elif [[ "$arg1" == "rm" && -n "$arg2" ]]; then
     echo "Not found"
   fi
 elif [[ "$arg1" == "-m" && -n "$arg2" ]]; then
-  echo "not implemented"
-  
+  strike=$'\u0336'
+  task_name="$arg2"
+  result=""
+  shift 2
+  for arg in "$@"; do
+    task_name+=" $arg"
+  done
+  grep -q "$task_name" "$task_file"
+  if [ $? -eq 0 ]; then
+    for(( i=0; i<${#task_name}; i++ )); do
+      result+="${task_name:$i:1}$strike"
+    done
+    sed -i "/$task_name/d" ./todo.txt
+    echo "$result" >> "$task_file"
+    echo "$result"
+  else
+    echo "task not found: 'todo ls' to list all tasks"
+  fi
 else
   echo "Command not recognized. Type 'todo --help' to view all commands."
 fi 
+
+
+
+
 

9bd94cb93ccad79c8c4e77594a32cfc3eeafb700
BishwasXD <gautambishwas7@gmail.com>
2025-01-12 22:00:08 +0545

    feat: added task priority colors

--- bin/todo.sh
+++ bin/todo.sh
@@ -4,9 +4,16 @@ task_file="$HOME/.todo.txt"
 if [ ! -f "$task_file" ]; then
     touch "$task_file"
 fi
+
 arg1="$1"
 arg2="$2"
 arg3="$3"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+DEFAULT_COLOR="$GREEN"
+
 if [[ "$arg1" == "ls" && -z "$arg2" ]]; then 
   while read -r line; do
     task="$line"
@@ -14,12 +21,27 @@ if [[ "$arg1" == "ls" && -z "$arg2" ]]; then
   done < "$task_file"
 
 elif [[ "$arg1" == "add" && -n "$arg2" ]]; then
-  task_name="$arg2"
-  shift 2
-  for arg in "$@"; do
-    task_name+=" $arg"
-  done
-  echo "$task_name" >> "$task_file"
+  if [[ "$arg2" == "-h" || "$arg2" == "-l" || "$arg2" == "-m" ]]; then
+      task_name="$arg3"
+      shift 3
+      for arg in "$@"; do
+        task_name+="$arg"
+      done
+      if [ "$arg2" == "-h" ]; then
+        echo -e "${RED}$task_name" >> "$task_file"
+      elif [ "$arg2" == "-l" ]; then
+        echo -e "${GREEN}$task_name" >> "$task_file"
+      elif [ "$arg2" == "-m" ]; then
+        echo -e "${YELLOW}$task_name" >> "$task_file"
+      fi
+  else
+    task_name="$arg2"
+    shift 2
+    for arg in "$@"; do
+      task_name+=" $arg"
+    done
+    echo -e "${DEFAULT_COLOR}$task_name" >> "$task_file"
+  fi
   echo "new task added: $task_name"
 
 elif [[ "$arg1" == "--help" && -z "$arg2" ]];then
@@ -78,7 +100,6 @@ elif [[ "$arg1" == "rm" && "$arg2" == "-m" && -n "$arg3" ]]; then
     echo "task not found: 'todo ls' to list all tasks"
   fi 
 else
-
   echo "Command not recognized. Type 'todo --help' to view all commands."
 fi
 
